61b3289b20a0ed52baadb61094c8b93c6da2e940
Simon Schick <demwizzy@gmail.com>
2018-03-27 20:56:09 +0200

    fixup! chore(targets): remove node v6 support

--- typings.d.ts
+++ typings.d.ts
@@ -1,36 +1,43 @@
 declare namespace Limitus {
     export interface LimitusExtendOptions {
-    get(key: string, callback: (err: Error, value?: string) => void): void;
-    set(key: string, value: string, expiration?: number, callback?: (err?: Error) => void): void;
+        get(key: string, callback: (err: Error, value?: string) => void): void;
+        set(key: string, value: string, expiration?: number, callback?: (err?: Error) => void): void;
     }
 
     export interface DropInfo {
-    count: number;
-    bucket: number;
+        count: number;
+        bucket: number;
     }
 
     export interface ModeResult {
-    limited: boolean;
-    next: string;
-    expiration: number;
-    info?: DropInfo;
+        limited: boolean;
+        next: string;
+        expiration: number;
+        info?: DropInfo;
     }
 
     export type ModeFn = (rule: Rule, value: string) => ModeResult;
 
     export interface Rule {
-    max: number;
-    interval: number;
-    mode?: 'continuous' | 'interval' | ModeFn;
+        max: number;
+        interval: number;
+        mode?: 'continuous' | 'interval' | ModeFn;
     }
 
     export class Rejected extends Error {
-
+        public readonly info: DropInfo;
     }
+
+    export type Ident = object | string | number;
 }
 declare class Limitus {
     public extend(obj: Limitus.LimitusExtendOptions): this;
     public rule(name: string, rule: Limitus.Rule): this;
-    public drop(name: string, ident: object, rule?: Limitus.Rule)
+
+
+    public drop(name: string, ident: Limitus.Ident, rule?: Limitus.Rule): Promise<Limitus.ModeResult>;
+    public drop(name: string, ident: Limitus.Ident, rule?: Limitus.Rule, callback?: (err: Error | null, res?: Limitus.ModeResult) => void): void;
+    public checkLimited(name: string, ident: Limitus.Ident, rule?: Limitus.Rule): Promise<Limitus.ModeResult>;
+    public checkLimited(name: string, ident: Limitus.Ident, rule?: Limitus.Rule, callback?: (err: Error | null, res?: Limitus.ModeResult) => void): void;
 }
 export = Limitus;
